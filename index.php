4109c5fe926dc77377e4c355de4360946e184ec2
Miguel Valdez <miguelvaldez35@gmail.com>
2016-09-20 19:39:07 -0400

    First upload of website
    Most of the website, still need to add projects and blog

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php
+//index.php 
+ 
+
+?>
+ 
+<html>
+    <head>
+
+        <title>mvg35</title>
+
+        <meta name="author" content="Miguel Valdez">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="/sass/css/style.css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    </head>
+
+    <body>
+
+    	<div id = "header">
+    		<nav class = "navbar navbar-default" role ="navigation">
+    			<div class="navbar-header wb">
+    				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+    					<span class="sr-only">Toogle Navigation</span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    				</button>
+    				<a class="navbar-brand" href="/">mvg35</a>
+    			</div>
+
+    			<div class="navbar-collapse navbar-ex1-collapse collapse wb" style="height: auto;">
+    				<ul>
+    					<li><a href="#Home">HOME</a></li>
+    					<li><a href="#About">ABOUT</a></li>
+    					<li><a href="#Work">WORK</a></li>
+    					<li><a href="#Blog">BLOG</a></li>
+    					<li><a href="#Resume">RESUME</a></li>
+    					<li><a href="http://www.instagram.com/miguel_24v/" 
+    						 target="_blank">
+							<img src="../media/instagram-bnw.png" alt="instagram" width="30" height="30">
+							</a>
+						</li>
+    					<li><a href="http://www.facebook.com/miguel.valdez.526"
+    						 target="_blank">
+							<img src="../media/fb-bnw.jpg" alt="facebook" width="30" height="30">
+							</a>
+						</li>
+						<li><a href="https://github.com/miguel24v"
+							 target="_blank">
+							<img src="../media/github-bnw.png" alt="github" width="30" height="30">
+							</a>
+						</li>
+						<li><a href="http://www.linkedin.com/in/miguelvaldez35"
+							 target="_blank">
+							<img src="../media/linkedIn-bnw.png" alt="linkedIn" width="30" height="30">
+							</a>
+						</li>
+    				</ul>
+    			</div>
+    		</nav>	
+
+    	</div>
+
+
+    	<div id="content">
+    		<div id="Home">
+       			<div class="main-image normal-padding">
+    				<h1>Welcome_</h1>
+    			</div>
+    		</div>
+
+	    	<div id="About">
+	    		<div class="title normal-padding"><h3>About_</h3></div>
+
+	    		<div class="about col-sm-offset-2 col-sm-8">
+
+		    		<div class="profile-pic col-sm-6">
+				   		<img src="/media/miguel.jpg" class="img-responsive">
+				   	</div>
+
+					<div class="about-me col-sm-6" style="margin-top:2em;">
+			   			<p>My name is Miguel Valdez. <br>
+				   			20 years old studying CS at Boston University. <br>
+				   			From Ciudad Juarez, Chihuahua, Mexico <br>	
+				   			Learning to code. <br>
+					   		“I think it is possible for ordinary people to choose to be extraordinary.”<br> - E. Musk
+						</p>
+				   	</div>
+			   	</div>
+	    	</div>
+
+	    	<div class="clearfix"></div>
+
+	    	<div id="Work">
+	    		<div class="title normal-padding"><h3>Work_</h3></div>
+
+	    			<div class="example phidev col-sm-8 col-sm-offset-2" style="margin-top:2em;">
+	    				<div class="description"></div>
+	    			</div>
+
+	    			<div class="example google col-sm-8 col-sm-offset-2">
+	    				<div class="description"></div>
+	    			</div>
+
+	    			<div class="example research col-sm-8 col-sm-offset-2">
+	    				<div class="description"></div>
+	    			</div>
+
+	    			<div class="clearfix"></div>
+
+	    	</div>
+
+	    	<div class="clearfix"></div>
+
+	    	<div id="Blog">
+	    		<div class="title normal-padding"><h3>Blog_</h3></div>
+
+	    			<div class="fixed-height-blog wb col-sm-8 col-sm-offset-2">
+						<p>
+
+
+						</p>
+					</div>
+
+					<div class="clearfix"></div>
+	    	</div>
+
+	    	<div class="clearfix"></div>
+
+	    	<div id="Resume">
+	    		<div class="title normal-padding"><h3>Resume_</h3></div>
+
+	    		<div class="resume" style="padding-top:2em; padding-bottom:2em;">
+	    			<h4><a href="documents/Miguel_Valdez_Resume.pdf" target="_blank">View</a> or <a href="/documents/Miguel_Valdez_Resume.pdf" download="mv35_resume.pdf">download</a> my updated Resume</h4>
+	    		</div>
+	    		<div class="clearfix"></div>
+
+	    	</div>
+    	</div>
+
+
+    	<div id="footer">
+    		<div class="row wb" style="padding-top:3em; padding-bottom:3em;">
+
+    			<div class="contact-info wb col-sm-4">
+    				<a href="mailto:mvg35@bu.edu"><h5>mvg35@bu.edu</h5></a>
+       			</div>
+
+       			<div class="group_social wb col-sm-4">
+       				<ul>
+       					<li class="social-image">
+       						<a href="http://www.instagram.com/miguel_24v/" 
+	   						target="_blank">
+								<img src="../media/instagram-bnw.png" alt="instagram">					
+							</a>
+       					</li>
+
+       					<li class="social-image">
+       						<a href="http://www.facebook.com/miguel.valdez.526"
+	  						 target="_blank">
+								<img src="../media/fb-bnw.jpg" alt="facebook">		
+							</a>
+       					</li>
+       					
+       					<li class="social-image">
+       						<a href="http://www.github.com/miguel24v"
+						 target="_blank">
+							<img src="../media/github-bnw.png" alt="github">					
+						</a>
+       					</li>
+       					
+       					<li class="social-image">
+       						<a href="http://www.linkedin.com/in/miguelvaldez35"
+	   					 target="_blank">
+							<img src="../media/linkedIn-bnw.png" alt="linkedIn">
+						</a>
+       					</li>
+       				</ul>
+
+    			</div>
+
+    			<div class="top-button wb col-sm-4">
+    				<a href="#Home"><h5>^ Back to top ^</h5></a>
+    			</div>
+			</div>
+    	</div>
+
+
+    </body>
+</html>
