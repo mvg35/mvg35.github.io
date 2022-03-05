765cee87ada2c42c37a0cffb6b2242e0bc032ceb
Miguel <miguelvaldez35@gmail.com>
2022-03-05 13:53:57 -0700

    cc

--- scripts/clean.js
+++ scripts/clean.js
@@ -7,19 +7,31 @@ const config = [
             { text: 'Se les enviara un codigo de acceso para la puerta principal de la casa. Solo se podra usar una vez asi que asegurese de dejar una puerta abierta mientras esta ahi. La puerta de enfrente se cierra sola despues de 60 segundos de estar cerrada' },
         ]
     },
-    { section_name: 'Iniciales', tasks: [
-        { text: 'Favor de asegurarse que este la tarjetita escrita y la otra. Favor de decirme si una no esta, la segunda es muy importante', content: 'entrada.png' },
-        { text: 'Visitar cada cuarto y empezar con la lavadera de las camas que hayan sido usadas. Esto es critico ya que es lo que mas tiempo tomara', content: 'lavadora.png' },
-        { text: 'Trate de incluir a la lavadora (o hacer una especial) para los trapos que se hayan usado en la limpieza pasada, asi para irlos rotando' },
-        { text: 'En este gabinete al fondo encontrara esta llave, la cual abre el cuartito de atras donde esta todo lo necesario para la limpieza', content: 'cuartito_key.png' },
-        { text: 'En el cuartito de atras tratamos de tener las cosas organizadas, tome todo lo que vaya a requerir y al finalizar regreselas a su lugar por favor', content: 'cuarto_limpieza.png' },
-    ] },
-    { section_name: 'Sala', tasks: [
-        { text: 'Limpiar, verificar, que esten bien y que esten en especial: Tele, Google Cast, Control, Decoraciones', content: 'sala_1.png' },
-        { text: 'Limpiar sofa, verificar que no haya manchas o rotaduras en cogines o sofa', content: 'sala_2.png' },
-        { text: 'Mover sofa, y verificar que esta extension este conectada y que tenga ese aparatito conectado', content: 'candado.jpg' },
-        { text: 'Verificar camara que esta en la ventana cerca de la palmera' },
-    ] }
+    {
+        section_name: 'Iniciales', tasks: [
+            { text: 'Favor de asegurarse que este la tarjetita escrita y la otra. Favor de decirme si una no esta, la segunda es muy importante', content: 'entrada.png' },
+            { text: 'Visitar cada cuarto y empezar con la lavadera de las camas que hayan sido usadas. Esto es critico ya que es lo que mas tiempo tomara', content: 'lavadora.png' },
+            { text: 'Trate de incluir a la lavadora (o hacer una especial) para los trapos que se hayan usado en la limpieza pasada, asi para irlos rotando' },
+            { text: 'En este gabinete al fondo encontrara esta llave, la cual abre el cuartito de atras donde esta todo lo necesario para la limpieza', content: 'cuartito_key.png' },
+            { text: 'En el cuartito de atras tratamos de tener las cosas organizadas, tome todo lo que vaya a requerir y al finalizar regreselas a su lugar por favor', content: 'cuarto_limpieza.png' },
+        ]
+    },
+    {
+        section_name: 'Sala', tasks: [
+            { text: 'Limpiar, verificar, que esten bien y que esten en especial: Tele, Google Cast, Control, Decoraciones', content: 'sala_1.png' },
+            { text: 'Limpiar sofa, verificar que no haya manchas o rotaduras en cogines o sofa', content: 'sala_2.png' },
+            { text: 'Verificar camara, que este conectada y prendide, esta en la ventana cerca de la palmera' },
+            { text: 'Mover sofa, y verificar que esta extension este conectada y que tenga ese aparatito conectado', content: 'candado.jpg' },
+        ]
+    },
+    {
+        section_name: 'Comedor', tasks: [
+            { text: 'Verificar que la camara de la puerta trasera este bien y conectada'},
+            { text: 'Verificar que la el centro de mesa tenga lo princial: sal, pimienta, '},
+            { text: 'Verificar que la mesa y sillas no tengan algun daño'},
+            { text: 'Verificar que esten las 4 decoraciones, 3 patitos', content: 'comedor.jpg' },
+        ]
+    },
 ];
 
 class Tabs extends React.Component {
@@ -127,5 +139,3 @@ ReactDOM.render(
     </div>,
     document.getElementById('react-div')
 );
-
-
